e9128a0953db5d0187515dd79ad186f62c40b5ba
Aragorn2209 <ramonbust2209@gmail.com>
2018-01-29 20:02:25 -0600

    Tarea 1 Algoritmos

---
+++ Tasks/Tareas/Tarea1-Algoritmos/cmake-build-debug/CMakeFiles/Tarea1_Algoritmos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea1_Algoritmos.dir/main.cpp.obj"
+  "Tarea1_Algoritmos.pdb"
+  "Tarea1_Algoritmos.exe"
+  "Tarea1_Algoritmos.exe.manifest"
+  "libTarea1_Algoritmos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea1_Algoritmos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
